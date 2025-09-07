3bee7c2448974b0f089256364b3d029a1fdf8818
Vincent Granville <vincentg@mltechniques.com>
2025-09-06 20:01:47 -0700

    Update transformed_eta.py

--- Source-Code/transformed_eta.py
+++ Source-Code/transformed_eta.py
@@ -1,4 +1,4 @@
-from scipy.optimize import fsolve, minimize, Bounds
+from scipy.optimize import fsolve
 import numpy as np
 
 import matplotlib.pyplot as plt
@@ -10,15 +10,17 @@ plt.rcParams['ytick.labelsize'] = 8
 
 #--- 1. Precomputations
 
-def pre_compute(swap_primes, del_primes):
+def pre_compute(swap_primes, del_primes, N, mode):
 
-    alog = np.zeros(N)
-    arr_omega = np.zeros(N)
-    arr_delta = np.zeros(N)
+    p_alog = np.zeros(N)
+    p_arr_omega = np.zeros(N)
+    p_arr_delta = np.zeros(N)
     flag = 1 
 
     for k in range(1, N):
 
+        if k % 100 == 0:
+            print("pre_compute:", k)
         hash_pval = {}
         new_k = k
         for p in swap_primes:
@@ -31,20 +33,39 @@ def pre_compute(swap_primes, del_primes):
         for p in hash_pval:
             new_k *= (swap_primes[p])**hash_pval[p]
 
-        alog[k] = np.log(new_k)
-        arr_omega[k] = new_k
-        arr_delta[k] = flag
+        p_alog[k] = np.log(new_k)
+        p_arr_omega[k] = new_k
+        p_arr_delta[k] = flag
         flag = -flag
 
         for p in del_primes:
             if k % p == 0:
-                arr_delta[k] = 0
+                p_arr_delta[k] = 0
 
-    return(alog, arr_omega, arr_delta)
+    if mode == 'sorted': 
+        ranks = np.abs(p_arr_omega).argsort().argsort()
+        alog = np.zeros(N)
+        arr_omega = np.zeros(N)
+        arr_delta = np.zeros(N)
+        for k in range(len(arr_omega)):
+            rank = ranks[k]
+            alog[rank] = p_alog[k]
+            arr_omega[rank] = p_arr_omega[k]
+            arr_delta[rank] = p_arr_delta[k]
+        return(alog, arr_omega, arr_delta)
+    else: 
+        return(p_alog, p_arr_omega, p_arr_delta)
 
-def eta2(sigma, t):
+
+def eta2(s, params):  ############### pass arguments ........
 
     # t is imaginary part
+    sigma = s[0]
+    t = s[1]
+    n = params[0]
+    alog = params[1]
+    arr_omega  = params[2]
+    arr_delta = params[3]
     vexp = np.exp(t*alog[1:n]*1j) 
     vpow = arr_delta[1:n] * arr_omega[1:n]**(-sigma) 
     sum = np.dot(vexp, vpow)
@@ -55,52 +76,69 @@ def eta2(sigma, t):
 
 sigma = 0.5 
 t = 25.010858 # 0.5 + it is 3rd root of zeta 
+s = [sigma, t]
 n0 = 4
 n1 = 3500
 N = 500001
 swap_primes = {} 
+mode = 'unsorted' # options: 'sorted', 'unsorted' 
 
-del_primes = (3, 5, 7) 
-m = len(del_primes) + 1
-alog, arr_omega, arr_delta = pre_compute(swap_primes, del_primes)
-for k in range(1, 40):
-    if arr_delta[k] != 0:
-        print("%3d %12.7f" %(k, arr_omega[k]*arr_delta[k]))
+del_primes1 = (3, 5, 7) 
+m1 = len(del_primes1) + 1
+alog, arr_omega, arr_delta = pre_compute(swap_primes, del_primes1, N, mode)
+##for k in range(1, 40):
+##    if arr_delta[k] != 0:
+##        print("%3d %12.7f" %(k, arr_omega[k]*arr_delta[k]))
+params1 = [0, alog, arr_omega, arr_delta]
+
+del_primes2 = () 
+m2 = len(del_primes2) + 1
+alog, arr_omega, arr_delta = pre_compute(swap_primes, del_primes2, N, mode)
+params2 = [0, alog, arr_omega, arr_delta]
 
 arr_x = []
-arr_y = []
+arr_y1 = []
+arr_y2 = []
 for n in range(n0, n1):
+    params1[0] = n
+    params2[0] = n
     arr_x.append(n-1)
-    arr_y.append(eta2(sigma, t))
+    arr_y1.append(eta2(s, params1)) 
+    arr_y2.append(eta2(s, params2)) 
 
-del_primes = ()
-alog, arr_omega, arr_delta = pre_compute(swap_primes, del_primes)
-arr_z = []
-for n in range(n0, n1):
-    arr_z.append(eta2(sigma, t))
-
-plt.plot(arr_x, arr_y, c='green', linewidth = 0.3, label="m = %d" %m)  
-plt.plot(arr_x, arr_z, c='red', linewidth = 0.6, label="m = 1")
+plt.scatter(arr_x, arr_y1, c='green', s = 0.1, label="m = %d" %m1)  
+plt.plot(arr_x, arr_y2, c='red', linewidth = 0.6, label="m = %d" %m2)
 plt.axhline(y=0.0, color='black', linestyle='dotted', linewidth = 0.3) 
 plt.legend(loc="upper right")
 plt.show()
 
+##alog = params1[1]
+##arr_omega = params1[2]
+##arr_delta = params1[3]
+##for k in range(n0, n1):
+##    print(k, alog[k], arr_omega[k], arr_delta[k]) 
+
 #--- 3. Changing primes in eta function
 
-def eta(s, n):
+def eta(s, params): 
     # s[0] = real part, s[1] = imaginary part
+    n = params[0]
+    alog = params[1]
+    arr_omega  = params[2]
+    arr_delta = params[3]
     vexp = np.exp(s[1]*alog[1:n]*1j) 
     vpow = arr_delta[1:n] * arr_omega[1:n]**(-s[0]) 
     sum = np.dot(vexp, vpow)
     return [sum.real, sum.imag]
 
 swap_primes = { 
-                  3: 4.051, # np.pi, 
-                  5: 7.916, # np.log(131), 
-                  7: 9.114 # np.exp(2)
+                  3: 4.051, 
+                  5: 7.916, 
+                  7: 9.114 
               } 
 del_primes = () 
-alog, arr_omega, arr_delta = pre_compute(swap_primes, del_primes)
+mode = 'unsorted' # options: 'sorted', 'unsorted' 
+alog, arr_omega, arr_delta = pre_compute(swap_primes, del_primes, N, mode)
 arr_x = []
 arr_y = []
 arr_z = []
@@ -108,13 +146,17 @@ arr_z = []
 sigma = 0.5
 t1 = 14.13472514
 t3 = 25.01085758
-for n in range(400, 100000, 1000):
+s1 = [sigma, t1]
+s3 = [sigma, t3]
+params = [0, alog, arr_omega, arr_delta]
+for n in range(400, 300000, 1000):
     arr_x.append(n-1)
-    arr_y.append(eta2(sigma, t1))
-    arr_z.append(eta2(sigma, t3))
+    params[0] = n
+    arr_y.append(eta2(s1, params)) 
+    arr_z.append(eta2(s3, params))
 
-plt.plot(arr_x, arr_y, c='green', linewidth = 0.3, label="s = s1")  
-plt.plot(arr_x, arr_z, c='red', linewidth = 0.6, label="s = s3")
+plt.plot(arr_x, arr_y, c='green', linewidth = 0.8, label="s = s1")  
+plt.plot(arr_x, arr_z, c='blue', linewidth = 0.8, label="s = s3")
 plt.axhline(y=0.0, color='black', linestyle='dotted', linewidth = 0.3) 
 plt.legend(loc="upper right")
 plt.show()
@@ -130,11 +172,12 @@ found2 = 0
 nfc = 0
 eps = 0.05
 
-for real in np.arange(0.45, 1.05, 0.001):
+params = [n, alog, arr_omega, arr_delta]
+for real in np.arange(0.45, 0.46, 0.001): ############ 1.05
     print("Real: %7.4f" %(real))
     for imag in np.arange(10.00, 40.01, 0.1):
         init = np.array([real, imag])
-        root = fsolve(eta, init, n)
+        root = fsolve(eta, init, params)
         areal.append(real)
         aimag.append(imag)
         sigma = min(1, root[0])
@@ -150,9 +193,64 @@ for real in np.arange(0.45, 1.05, 0.001):
         else:
             acolor.append('gold')
 
-plt.scatter(areal, aimag, s = 0.2, c=acolor)
+plt.scatter(aimag, areal, s = 0.2, c=acolor)
 plt.show()
 print()
 print("Found: left: %5.3f right: %5.3f" % (found1/nfc, found2/nfc))
+
+#--- 5. Replace all primes by random numbers
+
+from sympy import primerange, isprime
+
+def build_swap_primes(nprimes, noise):
+
+    arr_primes = list(primerange(3, nprimes))
+    swap_primes = {}
+    seed = 503
+    np.random.seed(seed)
+    N_max = len(arr_primes)
+    for k in range(N_max):
+        prime = arr_primes[k]
+        u = np.random.uniform(-noise, noise)
+        swap_primes[prime] = prime + u*(k**0.25) ##### k**0.25
+    print("N_max: ", N_max)
+    return(swap_primes, N_max)
+
+del_primes = ()
+nprimes = 300000
+sigma = 0.5 
+t = 25.010858 # 0.5 + it is 3rd root of zeta
+s = [sigma, t]
+
+noise1 = 0.005 
+swap_primes1, N_max = build_swap_primes(nprimes, noise1)
+mode = 'sorted' # options: 'sorted', 'unsorted'
+N = N_max  
+n0 = 4
+n1 = N_max
+
+alog, arr_omega, arr_delta = pre_compute(swap_primes1, del_primes, N, mode)
+params1 = [0, alog, arr_omega, arr_delta]
+
+noise2 = 0.000 
+swap_primes2, N_max = build_swap_primes(nprimes, noise2)
+alog, arr_omega, arr_delta = pre_compute(swap_primes2, del_primes, N, mode)
+params2 = [0, alog, arr_omega, arr_delta]
+
+arr_x = []
+arr_y1 = []
+arr_y2 = []
+for n in range(n0, n1):
+    params1[0] = n
+    params2[0] = n
+    arr_x.append(n)
+    arr_y1.append(eta2(s, params1))
+    arr_y2.append(eta2(s, params2))
+
+plt.scatter(arr_x, arr_y1, c='green', label="noise = %5.3f" %noise1, s=0.1) ### linewidth = 0.3)  
+plt.plot(arr_x, arr_y2, c='red', label="noise = %5.3f" %noise2, linewidth = 0.8)  
+plt.axhline(y=0.0, color='black', linestyle='dotted', linewidth = 0.3) 
+plt.legend(loc="upper right")
+plt.show()
 
 
